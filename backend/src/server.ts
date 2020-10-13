d2edb2dc18827984c48faddf32831fd197f6f706
João Vinaud <leyenasd@gmail.com>
2020-10-13 13:42:10 -0300

    Chore: Configure database and migrations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+import './database/connection';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 //import routes from './routes';
 //import cors from 'cors';
 
@@ -8,7 +11,35 @@ app.use(express.json());
 //app.use(cors());
 //app.use(routes);
 
+app.post('/orphanages', async(request, response) => {
+    
+    console.log(request.body);
 
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = request.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+    
+    return response.json({message: "hello world"});
+    });
 
 //exemplo hello world
 /*app.get('/', (request, response) => {
