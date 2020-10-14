d189676164103b49a7db6240808e304728cc3510
João Vinaud <leyenasd@gmail.com>
2020-10-14 17:26:52 -0300

    Fix: Fix use of CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,11 +9,13 @@ import routes from './routes';
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes)
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
-app.use(cors);
+
 
 //exemplo hello world
 /*app.get('/', (request, response) => {
