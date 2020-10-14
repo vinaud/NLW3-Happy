33d66c022d786ecba98aafdcaff1225034034e52
João Vinaud <leyenasd@gmail.com>
2020-10-14 09:28:33 -0300

    Chore: Add marker to map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -6,9 +6,16 @@ import '../styles/pages/orphanages-map.css';
 import mapMarkerImg from '../images/map-marker.svg';
 
 import { Link }  from  'react-router-dom';
-import {FiPlus} from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import {FiPlus, FiArrowRight} from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet';
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58,68],
+  iconAnchor: [29, 68],
+  popupAnchor: [178, 2]
+})
 
 function OrphanagesMap() {
   return (
@@ -36,6 +43,15 @@ function OrphanagesMap() {
         style={{width: '100%', height: '100%'}}
         >
           <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+          <Marker 
+          icon={mapIcon}
+          position={[-5.811854,-35.2098363]}>
+            <Popup closeButton={false} minWidth={240} maxWidth={240} className='map-popup'>
+               Lar das crianças</Popup>
+               <Link to="">
+                 <FiArrowRight size={20} color="#fff"/>
+               </Link>
+          </Marker>
         </Map>
         <Link to="" className="create-orphanage">
            <FiPlus size={32}  color="#fff"></FiPlus>
