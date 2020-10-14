627c990b65f6473be7f57e3ad268335ad39eaf3f
João Vinaud <leyenasd@gmail.com>
2020-10-14 08:13:19 -0300

    Chore: Add API  validations

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import './database/connection';
 import path from 'path';
+import 'express-async-errors';
+import errorHandler from './errors/handler';
+import cors from 'cors';
 
 import routes from './routes';
-//import cors from 'cors';
+
 
 const app = express();
 app.use(express.json());
 app.use(routes)
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));;
-//app.use(cors());
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
+app.use(cors);
 
 //exemplo hello world
 /*app.get('/', (request, response) => {
