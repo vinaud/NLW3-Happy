befedddd9eca83cbdfa6a9dd79d74131de5f2e4e
João Vinaud <leyenasd@gmail.com>
2020-10-17 09:06:08 -0300

    Chore: Add Header component to Screens

---
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+
+export default function SelectMapPosition(){
+    return(
+        <View/>
+    )
+}
--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -15,6 +15,10 @@ export default function OrphanagesMap(){
          navigation.navigate('OrphanageDetails');
      };
 
+     function handleNavigateToCreateOrphanage(){
+         navigation.navigate('SelectMapPosition');
+  };
+
 
     return(
     <View style={styles.container}>
@@ -47,7 +51,7 @@ export default function OrphanagesMap(){
          </MapView>
          <View style={styles.footer}>
            <Text style={styles.footerText} > 2 orfanatos encontrados</Text>
-         <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+         <TouchableOpacity style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
            <Feather name="plus" size={20} color="#FFF"></Feather>
          </TouchableOpacity>
          </View>
